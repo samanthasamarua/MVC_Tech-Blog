83fa7afa371a12a4c82ae90c2f9020f05bcd3937
Samantha Samarua <samantha.samarua@gmail.com>
2024-03-23 21:02:20 +0930

    09 Updated post and comment route files

--- controllers/api/postRoute.js
+++ controllers/api/postRoute.js
@@ -28,7 +28,7 @@ router.get('/', async (req, res) => {
 // Route retrieves a specific post based on id
 router.get('/:id', async (req, res) => {
     try {
-        const postData = await post.findOne({
+        const postData = await post.findByPk({
             where: { id: req.params.id},
             attributes: ['id', 'title', 'text', 'date_created'],
             order: [['date_created', 'DESC']],
@@ -43,7 +43,7 @@ router.get('/:id', async (req, res) => {
         });
 
         if (!postData) {
-            res.status(404).json({ message: `no posts with id = ${req.params.id}` });
+            res.status(404).json({ message: `no post with id = ${req.params.id}` });
             return;
           }
 
@@ -53,8 +53,68 @@ router.get('/:id', async (req, res) => {
     }
 });
 
-// Route that creates a post
+// Route handler is responsible for creating a new post. Checks is user is logged in
 
-// Route that updates a post
+router.post('/', withAuth, async (req, res) => {
+    try {
+      const newPost = await post.create({
+        ...req.body,
+        user_id: req.session.user_id,
+      });
+  
+      res.status(200).json(newPost);
+    } catch (err) {
+      res.status(400).json(err);
+    }
+  });
 
-// Delete a post by id
+
+// Route handler is responsible for updating a post
+router.put('/:id', withAuth, async (req, res) => {
+    try {
+      // Find the post by its ID
+      const postData = await post.findByPk(req.params.id);
+  
+      // If the post doesn't exist, return a 404 Not Found error
+      if (!postData) {
+        return res.status(404).json({ message: 'Post not found' });
+      }
+  
+      // Check if the current user is the author of the post
+      if (postData.user_id !== req.session.user_id) {
+        return res.status(403).json({ message: 'You are not authorized to update this post' });
+      }
+  
+      await postData.update(req.body);
+  
+      res.status(200).json(post);
+    } catch (err) {
+      res.status(400).json({ message: 'Failed to update post', error: err.message });
+    }
+  });
+
+
+
+// Route handler is responsible for deleting a post by id 
+
+router.delete('/:id', withAuth, async (req, res) => {
+    try {
+      const postData = await post.destroy({
+        where: {
+          id: req.params.id,
+          user_id: req.session.user_id,
+        },
+      });
+  
+      if (!postData) {
+        res.status(404).json({ message: 'No post found with this id!' });
+        return;
+      }
+  
+      res.status(200).json(postData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
+  module.exports = router;
